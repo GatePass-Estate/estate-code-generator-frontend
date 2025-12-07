f1750024593dbf08dcae486dda6134ab90575bfb
itschu <palmerbideconcepts@gmail.com>
2025-12-07 15:45:12 +0100

    fix: add resizeMode and style to share icon for consistent display

--- src/components/web/AccessCodeCard.tsx
+++ src/components/web/AccessCodeCard.tsx
@@ -106,7 +106,7 @@ const CodeCard = ({ code, details, variant = 'prefer_not_to_say', timeLeftMinute
 				</div>
 
 				<div className="cursor-pointer" onClick={() => router.push(`/invite?code=${code}&estate_id=${estate_id}`)}>
-					<Image source={icons.shareIcon} />
+					<Image source={icons.shareIcon} resizeMode="contain" style={{ width: 24, height: 24 }} />
 				</div>
 
 				{confirmOpen && (
