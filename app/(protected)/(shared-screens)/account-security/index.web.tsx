dfd3a8e6a1a00eb10c1bfba6f9389c2cf54de428
Okhuomon Ajayi <okhuomonajayi54@gmail.com>
2026-04-21 15:57:18 +0100

    fix:cursor pointer to change password button

--- app/(protected)/(shared-screens)/account-security/index.web.tsx
+++ app/(protected)/(shared-screens)/account-security/index.web.tsx
@@ -76,59 +76,77 @@ export default function AccountSecurityWeb() {
 	}, []);
 
 	return (
-		<div className="flex h-full w-screen overflow-y-scroll bg-body">
-			{isLargeScreen && <WebSidebar routes={menuRoutes} onNavigate={(route) => router.push(route as any)} />}
-			<div className="web-body">
-				<div className={`flex flex-col ${isLargeScreen ? 'mt-20 px-4 w-full' : 'mt-5 px-5'}`}>
-					<Back type="short-arrow" />
+    <div className="flex h-full w-screen overflow-y-scroll bg-body">
+      {isLargeScreen && (
+        <WebSidebar
+          routes={menuRoutes}
+          onNavigate={(route) => router.push(route as any)}
+        />
+      )}
+      <div className="web-body">
+        <div
+          className={`flex flex-col ${isLargeScreen ? "mt-20 px-4 w-full" : "mt-5 px-5"}`}
+        >
+          <Back type="short-arrow" />
 
-					<div className="mt-10">
-						<div className="flex justify-between">
-							<h1 className={`${isLargeScreen ? 'text-4xl' : 'text-2xl font-ubuntu-medium'}`}>Account Security</h1>
-						</div>
-						<p className="text-xs font-inter-medium text-[#172024] mt-4 leading-[14px]">
-							Manage your security settings and update your password for quick secure access.
-						</p>
-					</div>
+          <div className="mt-10">
+            <div className="flex justify-between">
+              <h1
+                className={`${isLargeScreen ? "text-4xl" : "text-2xl font-ubuntu-medium"}`}
+              >
+                Account Security
+              </h1>
+            </div>
+            <p className="text-xs font-inter-medium text-[#172024] mt-4 leading-[14px]">
+              Manage your security settings and update your password for quick
+              secure access.
+            </p>
+          </div>
 
-					<div className="py-7 w-full mt-5">
-						<div className="flex flex-col gap-4 mb-1">
-							<div className="flex items-center rounded-lg px-5 bg-[#F7F9F9] h-14 w-full justify-between">
-								<span className="text-[15px] font-inter-regular text-[#172024]">Change Password</span>
-								<div
-									onClick={() => {
-										setError('');
-										setSuccess('');
-										setShowUpdatePassword(true);
-									}}
-									className="cursor-pointer"
-								>
-									<Image source={icons.rightIcon} style={{ width: 14, height: 14 }} resizeMode="contain" />
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			{showUpdatePassword && (
-				<UpdatePassword
-					setShowUpdatePassword={setShowUpdatePassword}
-					error={error}
-					success={success}
-					savingPassword={savingPassword}
-					setPassword={setPassword}
-					password={password}
-					showCurrent={showCurrent}
-					showNew={showNew}
-					showConfirm={showConfirm}
-					confirmPasswordRef={confirmPasswordRef}
-					setError={setError}
-					setShowCurrent={setShowCurrent}
-					setShowNew={setShowNew}
-					setShowConfirm={setShowConfirm}
-					setNewPassword={setNewPassword}
-				/>
-			)}
-		</div>
-	);
+          <div className="py-7 w-full mt-5">
+            <div className="flex flex-col gap-4 mb-1">
+              <div
+                onClick={() => {
+                  setError("");
+                  setSuccess("");
+                  setShowUpdatePassword(true);
+                }}
+                className="cursor-pointer flex items-center rounded-lg px-5 bg-[#F7F9F9] h-14 w-full justify-between"
+              >
+                <span className="text-[15px] font-inter-regular text-[#172024]">
+                  Change Password
+                </span>
+                <div className="">
+                  <Image
+                    source={icons.rightIcon}
+                    style={{ width: 14, height: 14 }}
+                    resizeMode="contain"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {showUpdatePassword && (
+        <UpdatePassword
+          setShowUpdatePassword={setShowUpdatePassword}
+          error={error}
+          success={success}
+          savingPassword={savingPassword}
+          setPassword={setPassword}
+          password={password}
+          showCurrent={showCurrent}
+          showNew={showNew}
+          showConfirm={showConfirm}
+          confirmPasswordRef={confirmPasswordRef}
+          setError={setError}
+          setShowCurrent={setShowCurrent}
+          setShowNew={setShowNew}
+          setShowConfirm={setShowConfirm}
+          setNewPassword={setNewPassword}
+        />
+      )}
+    </div>
+  );
 }
